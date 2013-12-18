c9b5f4dcf7ed94a804fe2e8fe37224ecfbbb0500
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2013-12-18 18:32:46 -0500

    Updated

--- even.rb
+++ even.rb
@@ -1,8 +1,10 @@
+
+
 (1..10).each do |num|
 
 if num % 2 == 0
-  puts num + ' is an even number.'
+  puts "#{num} is an even number."
 else
-  puts num + ' is an odd number'
-  
-  num(2)
+  puts "#{num} is an odd number."
+end
+end
